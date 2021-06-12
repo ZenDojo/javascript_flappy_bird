18425e5602d95d8bb37ab82bade103f4e62fb8fb
ZenDojo <68330321+ZenDojo@users.noreply.github.com>
2021-06-12 16:25:06 -0700

    third

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import { getRandomNumber } from '/utils/utils.js'
+import { getRandomNumber, getCssProp, detectColision } from '/utils/utils.js'
 
-let game, block, hole, character, score, gameoverscreen, star, gameStopped, isJumping
+let game, block, hole, character, score, gameoverscreen, star, gameStopped, isJumping, scoreTotal
 
 function getElements() {
     game = document.querySelector('#game')
@@ -15,6 +15,7 @@ function getElements() {
 function setInitialValues() {
     gameStopped = false
     isJumping = false
+    scoreTotal = 0
 }
 
 function setEventListeners() {
@@ -22,12 +23,15 @@ function setEventListeners() {
         if (gameStopped) return
         resetAllAnimations()
     })
+    gameoverscreen.querySelector('button').addEventListener('click', () => {
+        location.reload()
+    })
     document.body.parentElement.addEventListener('click', () => {
         if (gameStopped) return
         characterJump()
     })
     document.onkeypress = e => {
-        e = e || window.event
+        // e = e || window.event
         if (e.keyCode === 32) {
             if (gameStopped) return
             characterJump()
@@ -35,34 +39,125 @@ function setEventListeners() {
     }
 }
 
-function characterJump(params) {
+function gameOver() {
+    (new Audio('/sounds/gameover.wav')).play()
+    gameStopped = true
+    showGameOverScreen()
+    stopBlockAnimation()
+    console.log('Game Over');
+}
+
+function showGameOverScreen() {
+    gameoverscreen.style.display = ''
+}
+
+function stopBlockAnimation() {
+    const blockLeft = block.getBoundingClientRect().x
+    block.style.animation = ''
+    hole.style.animation = ''
+
+    block.style.left = `${blockLeft}px`
+    hole.style.left = `${blockLeft}px`
+}
+
+function startBgAnimation() {
+    game.style.animation = 'backgroundAnimation 5s infinite linear'
+}
+
+function characterJump() {
     isJumping = true
+    let jumpCount = 0
+    const jumpInterval = setInterval(() => {
+        changeGameState({ diff: -4, direction: 'up' })
+        if (jumpCount > 20) {
+            (new Audio('/sounds/fly.wav')).play()
+            clearInterval(jumpInterval)
+            isJumping = false
+            jumpCount = 0
+        }
+        jumpCount++
+    }, 20);
+}
+
+function changeGameState({ diff, direction }) {
+    handleCharacterAnimation(direction)
+    handleCharacterCollisions()
+    handleCharacterPosition(diff)
+}
+
+function handleCharacterAnimation(direction) {
+    if (direction === 'down') {
+        character.classList.remove('go-up')
+        character.classList.add('go-down')
+    } else if (direction === 'up') {
+        character.classList.add('go-up')
+        character.classList.remove('go-down')
+    }
+}
+
+function changeScoreUi() {
+    score.innerText = `Score ${scoreTotal}`
+    gameoverscreen.querySelector('.score').innerText = score.innerText
+}
+
+function handleCharacterCollisions() {
+    const colisionBlock = detectColision(character, block)
+    const colisionHole = detectColision(character, hole, { y1: -46, y2: 47 })
+    if (colisionBlock && !colisionHole) {
+        return gameOver()
+    } else if (colisionHole) {
+        scoreTotal++
+        if (scoreTotal % 100 == 0) {
+            (new Audio('/sounds/hole.wav')).play()
+        }
+        changeScoreUi()
+    }
+}
+
+function handleCharacterPosition(diff) {
+    const characterTop = parseInt(getCssProp(character, 'top'))
+    const changeTop = characterTop + diff
+    if (changeTop < 0) {
+        return
+    }
+    if (changeTop > window.innerHeight) {
+        return gameOver()
+    }
+    character.style.top = `${changeTop}px`
 }
 
 function initRandomHoles() {
     hole.addEventListener('animationiteration', _ => {
-        const fromHeight = window.innerHeight * 60 / 100
-        const fromWeight = window.innerWidth * 95 / 100
-        const randomTop = getRandomNumber(fromHeight, fromWeight)
-        // const randomTop = Math.random() * 100
+        const fromHeight = 57 * window.innerHeight / 100
+        const toHeight = 97 * window.innerHeight / 100
+        const randomTop = getRandomNumber(fromHeight, toHeight)
         hole.style.top = `-${randomTop}px`
     })
 }
 
+function beginGravity() {
+    setInterval(() => {
+        if (isJumping || gameStopped) return
+        changeGameState({ diff: 5, direction: 'down' })
+    }, 20);
+}
+
 function resetAllAnimations() {
-    const seconds = 2
+    const seconds = 3
     const blockAnumationCss = `blockAnimation ${seconds}s infinite linear`
     block.style.animation = blockAnumationCss
     hole.style.animation = blockAnumationCss
-
 }
 
 function gameInit() {
     getElements()
     initRandomHoles()
+    beginGravity()
     resetAllAnimations()
     setInitialValues()
     setEventListeners()
+    startBgAnimation()
+    handleCharacterCollisions()
 }
 
 gameInit()
--- utils/utils.js
+++ utils/utils.js
@@ -1,21 +1,14 @@
-
-export const getRandomNumber = ( min, max ) => {
-    return Math.floor( Math.random() * (max - min) + min )
+export const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min)
 }
 
-export const getCssProp = ( element, cssProperty ) => {
+export const getCssProp = (element, cssProperty) => {
     return window
-        .getComputedStyle( element )
-        .getPropertyValue( cssProperty )
+        .getComputedStyle(element)
+        .getPropertyValue(cssProperty)
 }
 
-export const roundNum = ( num, decimals = 2 ) => {
-    return Math.round(( num + Number.EPSILON) * 10**decimals) / 10**decimals
-}
-
-export const l = msg => console.log( msg )
-
-export const detectColision = ( el1, el2, extra ) => {
+export const detectColision = (el1, el2, extra) => {
     const rect1 = el1.getBoundingClientRect()
     const rect2 = el2.getBoundingClientRect()
 
@@ -29,6 +22,6 @@ export const detectColision = ( el1, el2, extra ) => {
         rect1.x + rect1.width > rect2.x &&
         rect1.y < rect2.y + rect2.height + extra.y1 &&
         rect1.y + rect1.height > rect2.y + extra.y2
-    ) 
+    )
 
 }
